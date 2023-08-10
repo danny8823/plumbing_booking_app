ee3f9142d88c04c51fcac55c95f35404f0244de5
danny8823 <dannyyoo714@gmail.com>
2023-08-10 00:45:58 -0700

    added calender

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+const currentDate = new Date()
+const month = currentDate.getMonth()
 
+
+const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'cal-container'>
+        <h1 className = 'cal-month'>{months[month - 1]}</h1>
+        <div className = 'cal-day'>            
+            <div className = 'cal-date'>
+                <p>Sun</p>
+                <div>{currentDate.setDate(1)}</div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className = 'cal-date'>
+                <p>Mon</p>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className = 'cal-date'>
+                <p>Tue</p>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className = 'cal-date'>
+                <p>Wed</p>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className = 'cal-date'>
+                <p>Thur</p>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className = 'cal-date'>
+                <p>Fri</p>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className = 'cal-date'>
+                <p>Sat</p>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+
     </div>
   );
 }
