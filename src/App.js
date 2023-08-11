53be85bea86ee9152c06963d13bcf5ae192c4420
danny8823 <dannyyoo714@gmail.com>
2023-08-10 19:39:03 -0700

    Added new dynamic calendar with prev/next buttons

--- src/App.js
+++ src/App.js
@@ -1,74 +1,92 @@
-const currentDate = new Date()
-const month = currentDate.getMonth()
+import { useEffect, useState } from "react";
 
-
-const months = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-];
 function App() {
+    const [days, setDays] = useState([])
+    const [month, setMonth] = useState('')
+    let date = new Date();
+    let currentYear = date.getFullYear();
+    let currentMonth = date.getMonth();
+    
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    
+    const renderCalendar = () => {
+        let firstDay = new Date(currentYear, currentMonth, 1).getDay();
+        let lastDate = new Date(currentYear, currentMonth + 1, 0).getDate();
+        let lastDay = new Date(currentYear, currentMonth, lastDate).getDay();
+        let lastDateofLastMonth = new Date(currentYear, currentMonth, 0).getDate();
+
+        let render = []
+
+        for(let i = firstDay; i > 0; i--) {
+            render.push(<li className = 'inactive'>{lastDateofLastMonth - i + 1}</li>)
+        }
+
+        for( let i = 1; i <= lastDate; i++) {
+            let isToday = i === date.getDate() && currentMonth === new Date().getMonth() && currentYear === new Date().getFullYear() ? "active" : "";
+            render.push(<li className = {isToday}>{i}</li>)
+        }
+
+        for(let i = lastDay; i < 6; i++) {
+            render.push(<li className = "inactive">{i - lastDay + 1}</li>)
+        }
+
+        setDays(render)
+    }
+    
+    
+
+    const previousHandler = () => {
+        currentMonth = currentMonth - 1;
+        if (currentMonth < 0) {
+            currentYear = currentYear - 1;
+            currentMonth = 11;
+        }
+        date = new Date(currentYear, currentMonth, 1);
+        setMonth(months[currentMonth-1])
+        renderCalendar();
+    };
+
+    const nextHandler = () => {
+        currentMonth = currentMonth + 1;
+        if (currentMonth > 11) {
+            currentYear = currentYear + 1;
+            currentMonth = 0;
+        }
+        date = new Date(currentYear, currentMonth, 1);
+        setMonth(months[currentMonth-1])
+        renderCalendar();
+    };
+
+
+    useEffect(() => {
+        renderCalendar()
+        setMonth(months[currentMonth-1])
+    },[])
+
   return (
-    <div className = 'cal-container'>
-        <h1 className = 'cal-month'>{months[month - 1]}</h1>
-        <div className = 'cal-day'>            
-            <div className = 'cal-date'>
-                <p>Sun</p>
-                <div>{currentDate.setDate(1)}</div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div className = 'cal-date'>
-                <p>Mon</p>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div className = 'cal-date'>
-                <p>Tue</p>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div className = 'cal-date'>
-                <p>Wed</p>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div className = 'cal-date'>
-                <p>Thur</p>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div className = 'cal-date'>
-                <p>Fri</p>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div className = 'cal-date'>
-                <p>Sat</p>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+    <div className = 'container'>
+        <div className = 'title'>
+            <p>{month} {currentYear}</p>
+            <div className = 'icons'>
+                <span id = "prev" onClick ={previousHandler}>Prev</span>
+                <span id = "next" onClick = {nextHandler}>Next</span>
             </div>
         </div>
-
+        <div className = 'calendar'>
+            <ul className = 'weeks'>
+                <li>Sun</li>
+                <li>Mon</li>
+                <li>Tue</li>
+                <li>Wed</li>
+                <li>Thur</li>
+                <li>Fri</li>
+                <li>Sat</li>
+            </ul>
+            <ul className = 'days'>{days}</ul>
+        </div>
     </div>
   );
 }
